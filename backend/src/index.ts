aab1b0295e788039a2531187be5102c961c2c04c
Fanny Baquey <fanny.baquey@gmail.com>
2023-10-19 17:37:57 +0200

    Connexion à l'API de OPENAI

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,8 +2,10 @@ import app from "./app.js";
 import { connectToDatabase } from "./db/connections.js";
 
 // Server and listeners
+const PORT = process.env.PORT || 5000;
 connectToDatabase()
   .then(() => {
-    app.listen(5000, () => console.log("Server is running on port 5000 & Connected to Database"));
+    app.listen(PORT, () => 
+      console.log("Server is running on port 5000 & Connected to Database"));
   })
   .catch((err)=> console.log(err))
