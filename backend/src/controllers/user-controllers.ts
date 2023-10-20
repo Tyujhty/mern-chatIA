6d8cfaa4e537a1c202e08a4d59a16337f0782a86
Fanny Baquey <fanny.baquey@gmail.com>
2023-10-20 12:16:01 +0200

    Création du Controller userSignup

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,14 +1,45 @@
 import { NextFunction, Request, Response } from "express";
-import User from "../models/User.js"
+import User from "../models/User.js";
+import { hash } from "bcrypt";
 
-export const getAllUsers = async (req:Request, res:Response, next:NextFunction) => {
+export const getAllUsers = async (
+        req:Request, 
+        res:Response, 
+        next:NextFunction
+    ) => {
+        try {
+            const users = await User.find();
+
+            return res.status(200).json({
+                message: "OK", users
+            })
+        
+        } catch (error) {
+            console.log(error);
+            return res.status(200).json({
+                message: "ERROR", 
+                cause: error.message
+            })
+        }
+}
+
+export const userSignup = async (
+    req:Request, 
+    res:Response, 
+    next:NextFunction
+) => {
     try {
-        const users = await User.find();
+        const { name, email, password } = req.body;
+        const hashedPassword = await hash(password, 10) 
+        const user = new User({ name, email, password: hashedPassword });
+        
+        await user.save();
 
         return res.status(200).json({
-            message: "OK", users
+            message: "OK", 
+            id: user._id.toString()
         })
-
+    
     } catch (error) {
         console.log(error);
         return res.status(200).json({
--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
-import { getAllUsers } from "../controllers/user-controllers.js";
+import { 
+    getAllUsers, 
+    userSignup
+} from "../controllers/user-controllers.js";
 
 const userRoutes = Router();
 
 userRoutes.get("/", getAllUsers );
+userRoutes.post("/signup", userSignup )
+
 
 export default userRoutes;
